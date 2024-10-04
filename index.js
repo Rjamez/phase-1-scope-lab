0f324dab7673b0ff41c45ba7d85565031978873b
Rjamez <robin.adhola.student@moringaschool.com>
2024-10-04 16:40:23 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+
+function overwriteBestCustomer(newCustomer) {
+    bestCustomer = newCustomer;
+  }
+
+
+const leastFavoriteCustomer = 'bobby';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new customer';
+    leastFavoriteCustomer ='bobwell';
+  }
+
